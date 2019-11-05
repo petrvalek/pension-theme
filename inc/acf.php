b2eb120604e7568ac12c24c154853739e8def299
Petr Válek <valekpetr@outlook.cz>
2019-11-05 14:16:48 +0100

    Remove: ACF Google map API

--- inc/acf.php
+++ inc/acf.php
@@ -6,7 +6,7 @@
 
 function acf_map_key() {
 	
-	acf_update_setting('google_api_key', 'AIzaSyCReQXheHldmYl3ACT57fnMEg7SriX0uuQ');
+	// acf_update_setting('google_api_key', 'AIzaSyCReQXheHldmYl3ACT57fnMEg7SriX0uuQ');
 }
 
 add_action('acf/init', 'acf_map_key');
